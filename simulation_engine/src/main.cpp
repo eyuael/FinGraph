399db1438200bd7a3bc3db4a31a0171f74aec895
eyuael <eyuael.berhe@gmail.com>
2025-10-17 16:09:56 +0100

    files created

---
+++ simulation_engine/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from FinGraph CLI!" << std::endl;
+    return 0;
+}
---
+++ simulation_engine/tests/tests.cpp
@@ -0,0 +1,14 @@
+#include "../include/fingraph/Backtest.h"
+#include "../include/fingraph/MarketData.h"
+#include "../include/fingraph/PerformanceMetrics.h"
+#include "../include/fingraph/Portfolio.h"
+#include "../include/fingraph/Strategy.h"
+#include "../include/fingraph/Trade.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Running tests..." << std::endl;
+    // Add your test cases here
+    return 0;
+}
